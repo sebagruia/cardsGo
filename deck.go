c4a4a6800387ada84bfcb358e048c62403fc8422
sebagruia <blackthorus@gmail.com>
2022-01-11 19:30:18 +0200

    Add shuffle function

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of Deck
@@ -49,6 +51,16 @@ func (d deck) toByte() []byte {
 
 func (d deck) saveToFile(fileName string) error {
 	return ioutil.WriteFile(fileName, d.toByte(), 0644)
+}
+
+func (d deck) shuffle() deck {
+	source := rand.NewSource(time.Now().UnixNano())
+	r:= rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d)-1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+	return d
 }
 
 func newDeckFromFile(fileName string) deck {
--- main.go
+++ main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	cards := newDeckFromFile("SebaToDisk");
+	cards.shuffle()
 	cards.print()
 }
